121e5fc5139c8a5201e4ae7e5a774a1a8f046b9b
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-13 09:39:07 +0530

    adminlogin

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+
+  aname=""
+  psw=""
+
+  constructor(private route:Router){}
+
+
+  readValues=()=>
+  {
+    let data:any={"aname":this.aname,"psw":this.psw}
+    console.log(data)
+
+    if ( this.aname=="admin" && this.psw=="12345")
+
+    {
+      this.route.navigate([''])
+ 
+   }
+ 
+   else {
+ 
+     alert("invalid creditioanals")
+ 
+   }
+   
+    
+  }
+
+
+}
